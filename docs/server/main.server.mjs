38cf00d94caeb5f5c1e87c2d74e2a7e01095e49d
mnelshazly <mnelshazly7@gmail.com>
2024-08-04 13:26:53 +0300

    second commit

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-CMTIVFJE.mjs";import"./chunk-4E6GVGWL.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-3L5RUU64.mjs";import"./chunk-4E6GVGWL.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
